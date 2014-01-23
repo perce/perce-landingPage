1255fd635d7243469214d40b7350c9c4fce94d5c
christian.haeringer <christian.haeringer@project-a.com>
2014-01-23 10:51:21 +0100

    linted gulpfle

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // Gulpfile.js
 // Require the needed packages
 
+
 var gulp    = require('gulp');
 var stylus  = require('gulp-stylus');
 var refresh = require('gulp-livereload');
@@ -13,41 +14,46 @@ var server  = lr();
 
 // Get and render all .styl files recursively
 gulp.task('stylus', function(){
-    gulp.src('./assets/styles/**/*.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('./public/styles'))
-    .pipe(notify({
-      message: "New styles generated!"
-    }));;
+  'use strict';
+  gulp.src('./assets/styles/**/*.styl')
+  .pipe(stylus())
+  .pipe(gulp.dest('./public/styles'))
+  .pipe(notify({
+      message: 'New styles generated!'
+    }));
 });
 
 gulp.task('jade', function() {
+  'use strict';
   gulp.src('./templates/*.jade')
     .pipe(jade())
-    .pipe(gulp.dest('./public/'))
+    .pipe(gulp.dest('./public/'));
 });
 
 gulp.task('livereload', function(){
-    server.listen(35729, function(err){
-        if(err) return console.log(err);
-    });
+  'use strict';
+  server.listen(35729, function(err){
+    if(err) {
+      return console.log(err);
+    }
+  });
 });
 
 
 
 // Default gulp task to run
 gulp.task('default', function(){
-    gulp.run('livereload', 'stylus');
+  'use strict';
+  gulp.run('livereload', 'stylus');
+  gulp.run('stylus');
+  gulp.run('jade');
+
+  gulp.watch('./assets/styles/**/*.styl', function() {
     gulp.run('stylus');
+  });
+
+  gulp.watch('./templates/**/*.jade', function() {
     gulp.run('jade');
-
-    gulp.watch('./assets/styles/**/*.styl', function() {
-      gulp.run('stylus')
-    });
-
-    gulp.watch('./templates/**/*.jade', function() {
-      gulp.run('jade')
-    });
-
+  });
 
 });
